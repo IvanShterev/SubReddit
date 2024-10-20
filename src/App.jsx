ddf4ca9d0ad4d391a6d712dbdeeb244314caec52
Ivan Shterev <ivan.shterevv@gmail.com>
2024-10-20 15:56:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './navigation/nav'
+import LeftSidebar from './left-sidebar/left-sidebar'
+
+{/* https://www.reddit.com/r/AmItheAsshole/ */}
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <LeftSidebar />
+    </>
+  )
+}
+
+export default App
